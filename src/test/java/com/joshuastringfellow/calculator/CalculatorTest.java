13c07bbcd11043bc4a208ecd1c882133a64334ac
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2017-12-03 20:50:54 +0000

    Add test, comments, and display name
    Add simple example test
    Remove parameter from addTest
    Add DisplayName tags
    Add comment explaining static import

--- src/test/java/com/joshuastringfellow/calculator/CalculatorTest.java
+++ src/test/java/com/joshuastringfellow/calculator/CalculatorTest.java
@@ -1,14 +1,27 @@
 package com.joshuastringfellow.calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
 
+/*
+ * The static import allows unqualified access to assertEquals
+ * This means the method can be used as `assertEquals` instead
+ * of `Assertions.assertEquals`
+*/
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CalculatorTest {
+@DisplayName("Calculator Test Class")
+class CalculatorTest {
 
   @Test
-  void addTest(TestInfo testInfo) {
+  @DisplayName("Basic Unit Test")
+  void basicTest() {
+    assertEquals(1, 1);
+  }
+
+  @Test
+  @DisplayName("Basic Unit Test for Calculator add method")
+  void addTest() {
     Calculator calculator = new Calculator();
     assertEquals(2, calculator.add(1, 1));
   }
